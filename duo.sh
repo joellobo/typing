2bb5b7c37cd43f56be72c8299063867f3a465062
Joel Lobo <joellobo@gmail.com>
2019-06-01 17:56:56 -0300

    refactoring

--- duo.sh
+++ duo.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
+RANDOMICO=$1
+DB=$2
+
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'XYZ=($(cat db.txt))'
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'XYZ=($(cat pt-en.txt))'
 
-CONTADOR=0
 TOTAL=${#XYZ[@]}
+
+if [ "$RANDOMICO" = "R"  ]
+then
+  CONTADOR=`shuf -i 0-1000 -n 1`
+else
+  CONTADOR=0
+fi
 
 while :; do
 
   phrase_in_portuguese="$(cut -d';' -f1 <<<${XYZ[$CONTADOR]})"
   phrase_in_english="$(cut -d';' -f2 <<<${XYZ[$CONTADOR]})"
 
-  echo "Palavra $CONTADOR: '$phrase_in_portuguese' '$phrase_in_english'" 
+  printf "Palavra $CONTADOR: '$phrase_in_portuguese' " 
 
   read input_phrase_in_english
 
-  if [ "$phrase_in_english" = "$input_phrase_in_english" ]; 
+  if [ "${phrase_in_english^^}" = "${input_phrase_in_english^^}" ]; 
   then
-    echo "Correto"
+    printf "${GREEN}Correto${NC}\n"
   else
     printf "${RED}Errado${NC} A tradução correta é '$phrase_in_english'\n"
   fi
 
-  let CONTADOR=CONTADOR+1; 
+  if [ "$RANDOMICO" = "R"  ]
+  then
+    let CONTADOR=`shuf -i 0-1000 -n 1`
+  else
+    let CONTADOR=CONTADOR+1; 
+  fi
 
 done
